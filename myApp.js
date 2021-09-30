d6112c247935f920008871b1a63f2201a251e0d6
Milad Hazrati <miladhazrati75@gmail.com>
2021-09-30 16:58:59 +0330

    static

--- myApp.js
+++ myApp.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
-console.log("Hello World")
+console.log("Hello World");
+app.use(express.static(__dirname+"/public"));
 app.get("/",function(req,res){
  res.sendFile(__dirname+"/views/index.html");
 })
