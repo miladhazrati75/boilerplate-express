7f85e1d89b7d996db36642da3d2a52e79245cb81
Milad Hazrati <miladhazrati75@gmail.com>
2021-09-30 19:11:42 +0330

    middle

--- myApp.js
+++ myApp.js
@@ -5,7 +5,14 @@ app.use(function(req,res,next){
 console.log(`${req.method} ${req.path} - ${req.ip}`);
  next();
 })
+
 app.use("/public",express.static(__dirname+"/public"));
+app.get("/now",function(req,res,next){
+req.time=new Date().toString();
+ next();
+},function(req,res){
+ res.send({time:req.time})
+})
 app.get("/json",function(req,res){
  if (process.env.MESSAGE_STYLE=="uppercase"){
  res.send({"message": "HELLO JSON"} );
