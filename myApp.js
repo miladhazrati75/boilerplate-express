c2c67be12e62a205c553bbbc1f75b246f2e740c7
Milad Hazrati <miladhazrati75@gmail.com>
2021-09-30 18:06:25 +0330

    uppercase

--- myApp.js
+++ myApp.js
@@ -3,7 +3,7 @@ var app = express();
 console.log("Hello World");
 app.use("/public",express.static(__dirname+"/public"));
 app.get("/json",function(req,res){
- res.send({"message": "Hello json"} );
+ res.send(process.env.MESSAGE_STYLE=="uppercase"?{"message": "HELLO JSON"}:{"message": "Hello json"} );
 })
 app.get("/",function(req,res){
  res.sendFile(__dirname+"/views/index.html");
